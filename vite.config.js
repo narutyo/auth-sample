377cb93bc887838612a15acb66383a86d967f056
narutyo <narutyo@narutyo.work>
2022-11-23 20:51:20 +0900

    Passport,vite,認証画面実装

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+      host: true,
+      port: 55173,
+      hmr: {
+        host: "localhost",
+      },
+    },
 });
